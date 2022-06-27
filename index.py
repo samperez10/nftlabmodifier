305ab0343513cf6ddb7e8ae89d1d2320fff98331
samperez10 <sam.perez10@outlook.com>
2022-06-27 10:30:36 +0800

    added boki

--- index.py
+++ index.py
@@ -31,8 +31,6 @@ def bokizombie(id):
   
   js["image"] = f"https://nftstorage.link/ipfs/bafybeiexnomle7nzrdjgejs4gllgb6tl32nkixmhvofhagzzdx2pp6pr7a/{id}.png"
   
-  js["description"] = "Caked Apes Flip are 2222 unique and randomly generated NFTs living in Polygon Blockchain."
-  
   return jsonify(js)
 
 @app.route("/")
